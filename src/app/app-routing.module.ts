8daa0f0c77ad532d4f61f78401d6783a51c26059
Fabiancitto <fabi.fica@duocuc.cl>
2024-09-25 21:35:16 -0300

    aplicacion app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
+  { path: 'role-selection', loadChildren: () => import('./pages/role-selection/role-selection.module').then(m => m.RoleSelectionPageModule) },
+  { path: 'pasajero', loadChildren: () => import('./pages/pasajero/pasajero.module').then(m => m.PasajeroPageModule) }, 
+  { path: 'conductor', loadChildren: () => import('./pages/conductor/conductor.module').then(m => m.ConductorPageModule) },
+  { path: 'perfil', loadChildren: () => import('./pages/perfil/perfil.module').then(m => m.PerfilPageModule) },
+  { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule) }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
